e8d74322ea7c41147ebc884a9022c91a21000f1e
Manifestest1 <mani!fes%test1@12>
2024-03-17 10:24:39 +0530

    'changes'

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,3 @@ Route::get('/', function ()
     return view('welcome');
 });
 
-
-Route::get('/login/google', function () 
-{
-    return Socialite::driver('google')->redirect();
-});
-
-Route::get('/login/google/callback', function () 
-{
-    $user = Socialite::driver('google')->user();
-    dd($user);
-    // Handle the authenticated user as needed
-});
